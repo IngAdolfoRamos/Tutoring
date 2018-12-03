c072714e872373ee41d74c1d6fb257eb17dbb19e
IngAdolfoRamos <luadramo@gmail.com>
2018-12-03 00:04:59 -0600

    Adding users for Roles seeder.

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -16,14 +16,29 @@ class RolesTableSeeder extends Seeder
         ([
             'name' => 'Admin',
             'slug' => 'admin',
-            'description' => null,
+            'description' => 'Have total access and control over the entire system',
             'special' => 'all-access'
         ]);
 
         Role::create
         ([
-            'name' => 'tutor',
+            'name' => 'Tutor',
             'slug' => 'tutor',
+            'description' => 'Can create and edit reports, and see all the information.',
+        ]);
+
+        Role::create
+        ([
+            'name' => 'Teacher',
+            'slug' => 'teacher',
+            'description' => 'Can see all the reports and all the information.',
+        ]);
+
+        Role::create
+        ([
+            'name' => 'Student',
+            'slug' => 'student',
+            'description' => 'Can see its own public academic information.',
         ]);
     }
 }
