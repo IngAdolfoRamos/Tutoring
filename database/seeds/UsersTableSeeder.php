692b87ab06f5ac120fd534767a5defd9da64a800
IngAdolfoRamos <luadramo@gmail.com>
2018-12-03 00:05:15 -0600

    Creating users for users seeder file.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,19 +12,43 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $adolfo = User::create
+        $janeli = User::create
         ([
-            'record' => '201514076',
-            'name' => 'Adolfo Ramos',
-            'email' => 'ingadolfo@icloud.com',
+            'record' => '201514080',
+            'name' => 'Janeli',
+            'email' => 'janeli@me.com',
+            'password' => bcrypt('secret')
+        ]);
+
+        $virginia = User::create
+        ([
+            'record' => '201514079',
+            'name' => 'Virginia Aguilar',
+            'email' => 'virginia@me.com',
+            'password' => bcrypt('secret')
+        ]);
+
+        $adan = User::create
+        ([
+            'record' => '201514078',
+            'name' => 'Adan',
+            'email' => 'adan@me.com',
+            'password' => bcrypt('secret')
+        ]);
+
+        $elizabeth = User::create
+        ([
+            'record' => '201514077',
+            'name' => 'Elizabeth',
+            'email' => 'elizabeth@me.com',
             'password' => bcrypt('secret')
         ]);
 
         $adolfo = User::create
         ([
-            'record' => '201514077',
-            'name' => 'Ismael Rayon',
-            'email' => 'isma@me.com',
+            'record' => '201514076',
+            'name' => 'Adolfo Ramos',
+            'email' => 'adolfo@me.com',
             'password' => bcrypt('secret')
         ]);
     }
