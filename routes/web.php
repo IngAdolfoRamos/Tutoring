fb4845ffcfdce5507ef52b772c1099b00065289a
IngAdolfoRamos <luadramo@gmail.com>
2018-12-03 11:29:28 -0600

    Adding routes for reports

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,30 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Authentication Routes...
+/* Authentication Routes... */
 $this->get('/', 'Auth\LoginController@showLoginForm')->name('login');
 $this->post('login', 'Auth\LoginController@login');
 $this->post('logout', 'Auth\LoginController@logout')->name('logout');
 
-// Registration Routes...
+/* Registration Routes... */
 $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 $this->post('register', 'Auth\RegisterController@register');
 
-// Password Reset Routes...
+/*// Password Reset Routes...
 $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
 $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-$this->post('password/reset', 'Auth\ResetPasswordController@reset');
+$this->post('password/reset', 'Auth\ResetPasswordController@reset');*/
 
+Route::middleware(['auth'])->group(function ()
+{
+    /* Reports */
+    Route::get('reports', 'ReportController@index')->name('reports.index')->middleware('permission:reports.index');
+    Route::get('reports/create', 'ReportController@create')->name('reports.create')->middleware('permission:reports.create');
+    Route::post('reports/store', 'ReportController@store')->name('reports.store')->middleware('permission:reports.create');
+    Route::get('reports/{report}', 'ReportController@show')->name('reports.show')->middleware('permission:reports.show');
+    Route::get('reports/{report}/edit', 'ReportController@edit')->name('reports.edit')->middleware('permission:reports.edit');
+    Route::put('reports/{report}', 'ReportController@update')->name('reports.update')->middleware('permission:reports.edit');
+    Route::get('reports/{report}/download', 'ReportController@download')->name('reports.download')->middleware('reports.download');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
