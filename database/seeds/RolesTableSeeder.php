deb712b08c265a8177c8de39ad7bf33dcabe54d9
IngAdolfoRamos <luadramo@gmail.com>
2018-12-02 23:10:07 -0600

    Creating seeder for Roles table.

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create
+        ([
+            'name' => 'Admin',
+            'slug' => 'admin',
+            'description' => null,
+            'special' => 'all-access'
+        ]);
+
+        Role::create
+        ([
+            'name' => 'tutor',
+            'slug' => 'tutor',
+        ]);
+    }
+}
