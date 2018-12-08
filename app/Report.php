573864c893745a173c3d751d321b763d4331941e
IngAdolfoRamos <luadramo@gmail.com>
2018-12-08 15:36:53 -0600

    Adding fields ro report model to avoid massAssignment exception

--- app/Report.php
+++ app/Report.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    protected $fillable = ['record', 'name', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
