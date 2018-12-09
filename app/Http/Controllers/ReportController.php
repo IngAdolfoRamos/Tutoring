0fe7c88ee5b9360890fffabedf715fa3d8fc2eab
IngAdolfoRamos <luadramo@gmail.com>
2018-12-09 15:35:05 -0600

    Adding reportController resource.

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Report;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Barryvdh\DomPDF\Facade as PDF;
+
+class ReportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('reports.index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function pdf()
+    {
+        /**
+         * toma en cuenta que para ver los mismos
+         * datos debemos hacer la misma consulta
+         **/
+        $users = User::all();
+
+        $pdf = PDF::loadView('reports.index', compact('users'));
+
+        return $pdf->download('users.pdf');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        $report = Report::find($id);
+        return view('reports.edit', compact('user','report'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        Report::updateOrCreate
+        ([
+            'record' => $request->record,
+        ],
+            [
+                'name' => $request->name,
+                'description' => $request->description,
+            ]
+        );
+
+//        'record' => $request->input('record')
+        //$report->update($request->all());
+
+
+        return redirect()->route('reports.index');
+//        return redirect()->route('reports.edit', $report->id)
+//            ->with('info', 'Producto actualizado con exito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
